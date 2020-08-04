2279ada23b1388b6705c7ae5bd67f466bfe82973
yushudi <hzj910416@163.com>
2020-08-04 22:58:35 +0800

    第二周作业

---
+++ Week_02/[429]N叉树的层序遍历-递归.java
@@ -0,0 +1,75 @@
+//给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其层序遍历: 
+//
+// [
+//     [1],
+//     [3,2,4],
+//     [5,6]
+//]
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 树的深度不会超过 1000。 
+// 树的节点总数不会超过 5000。 
+// Related Topics 树 广度优先搜索 
+// 👍 99 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) return res;
+        List<Node> roots = new ArrayList<>();
+        roots.add(root);
+        helper(roots, res);
+        return res;
+    }
+
+    private void helper(List<Node> nodes, List<List<Integer>> list) {
+        List<Integer> res = new ArrayList<>();
+        List<Node> nextLevel = new ArrayList<>();
+        for (Node node : nodes) {
+            res.add(node.val);
+            if (node.children != null)
+                nextLevel.addAll(node.children);
+        }
+        list.add(res);
+        if (nextLevel.size() > 0) {
+            helper(nextLevel, list);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ Week_02/[49]字母异位词分组.java
@@ -0,0 +1,48 @@
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。 
+//
+// 示例: 
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//] 
+//
+// 说明： 
+//
+// 
+// 所有输入均为小写字母。 
+// 不考虑答案输出的顺序。 
+// 
+// Related Topics 哈希表 字符串 
+// 👍 418 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (map.containsKey(key)) {
+                map.get(key).add(str);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
