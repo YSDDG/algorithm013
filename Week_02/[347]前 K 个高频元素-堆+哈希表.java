49f493a0bc343b94a2d1b6499fedbcec63d4ea6c
yushudi <hzj910416@163.com>
2020-08-05 14:28:18 +0800

    LeetCode 263 丑数 数学法 LeetCode 347 堆+哈希表 LeetCode 剑指Offer 49 丑数 堆法

---
+++ Week_02/[347]前 K 个高频元素-堆+哈希表.java
@@ -0,0 +1,54 @@
+//给定一个非空的整数数组，返回其中出现频率前 k 高的元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: nums = [1,1,1,2,2,3], k = 2
+//输出: [1,2]
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [1], k = 1
+//输出: [1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。 
+// 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。 
+// 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。 
+// 你可以按任意顺序返回答案。 
+// 
+// Related Topics 堆 哈希表 
+// 👍 416 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k < 1 || k > nums.length) return new int[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.merge(num, 1, Integer::sum);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            pq.add(e);
+            while (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = Objects.requireNonNull(pq.poll()).getKey();
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
